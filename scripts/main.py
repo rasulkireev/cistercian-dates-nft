5a8df8be9dd8da05473fb8a028b523f572ba9037
Rasul Kireev <rasul.kireev@guycarp.com>
2021-09-07 16:08:21 -0400

    final configurations

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from datetime import date, timedelta
 import time
+import logging
 from dotenv import load_dotenv
 
 from image import create_and_save_image
@@ -10,16 +11,17 @@ from utils import (
   load_file_to_Pinata,
   get_project_root
 )
+from mint_nft import mintNFT
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 def main():
 
     project_root = get_project_root()
-    start_date = date(1,1,1)
-    end_date = date(100,1,1)
-
+    start_date = date(14,1,1)
     current_date = start_date
+    end_date = date(14,1,2)
 
     count = 1
     while current_date <= end_date:
@@ -30,25 +32,33 @@ def main():
       image_path = os.path.join(project_root, "images", image_name)
       response = load_file_to_Pinata(image_path, image_name)
 
+      # save response hash to a txt file
+      with open(f"{project_root}/pinata_hashes.txt", 'a', encoding="utf-8") as file:
+        file.write("\n")
+        file.write(response['IpfsHash'])
+
       # Generate, Save and Upload Metadata for Unique Image
       metadata_file_name = create_and_save_metadata_file(current_date, response)
       metadata_file_path = os.path.join(project_root, "metadata", metadata_file_name)
       response = load_file_to_Pinata(metadata_file_path, metadata_file_name)
 
       # save response hash to a txt file
-      with open(f"{project_root}/metadata_files_hashes.txt", 'a', encoding="utf-8") as file:
+      with open(f"{project_root}/pinata_hashes.txt", 'a', encoding="utf-8") as file:
         file.write("\n")
         file.write(response['IpfsHash'])
 
+      response = mintNFT(f"https://gateway.pinata.cloud/ipfs/{response['IpfsHash']}")
+      logging.info(f'NFT #{count} is minted. Transaction Address: {response["transactionHash"].hex()}')
+
+      # estimating how long it will take to complete the rest
       executionTime = (time.time() - startTime)
       date_diff = (end_date - current_date).days
       time_left = (executionTime * date_diff)
-      print(f"File #{count} done. {date_diff} left. Estimated time: { time_left / 60 / 60 } / {time_left / 60} minutes / { time_left } seconds")
+
+      logging.info(f'NFT #{count} done. {date_diff} left. Estimated time: { time_left / 60 / 60 } hours / {time_left / 60} minutes / { time_left } seconds')
 
       count += 1
       current_date += timedelta(days=1)
-
-
 
 if __name__ == "__main__":
     startTime = time.time()
